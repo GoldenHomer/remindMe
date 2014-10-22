53f6868e69659eb0806d189692645dc7394450c4
Jose Contreras <jocontre@joselcontreras.com>
2014-10-21 22:52:22 -0500

    Auth and security

---
+++ server/logic/auth/auth.js
@@ -0,0 +1,34 @@
+var db = require('../../db/db.auth'),
+    crypt = require('./crypt'); // Hashes passwords
+ 
+var auth = {
+ 
+  register: function(user) {
+    // create a password hash
+    user.password = crypt.encryptPwd(user.password);
+ 
+    // save the credentials in the database
+    return db.register(user);
+ 
+  },
+ 
+  login: function(user) {
+    var dbUser = db.findUser(user.username);
+    if (dbUser) {
+      if (crypt.decryptPwd(user.password, dbUser.password)) {
+        delete dbUser.password; // Remove the password from response
+        return dbUser;
+      } 
+      else {
+        return -1;
+      }
+    } 
+    else {
+      return -2;
+    }
+ 
+  }
+ 
+};
+ 
+module.exports = auth;
