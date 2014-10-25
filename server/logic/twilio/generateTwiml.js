2ee313df8dae2ea251af756bb029578e77c33bf4
Jose Contreras <jocontre@joselcontreras.com>
2014-10-24 23:03:45 -0500

    DB stuff and Twilio logic

---
+++ server/logic/twilio/generateTwiml.js
@@ -0,0 +1,36 @@
+var db = require('../../db/db.reminder'),
+	client = require('./client'),
+	twilio = require('twilio');
+
+var twl = {
+  getTwiml: function(userid, reminderid) {
+ 
+    // we are going to use userid for authorization, if needed. 
+ 
+    var rem = db.getOne(reminderid);
+    var twiml = new twilio.TwimlResponse();
+ 
+    
+    var options = {
+      voice: 'woman',
+      language: 'en-us'
+    };
+    if (rem) {
+      twiml.say('This is a reminder for : ', options)
+        .pause({
+          length: 1
+        })
+        .say(rem.remindThis)
+        .pause({
+          length: 1
+        }).say('Thank you for using our service.', options);
+    } 
+    else {
+      twiml.say('What the...!', options);
+    }
+ 
+    return twiml.toString();
+  }
+}
+ 
+module.exports = twl;
---
+++ server/routes/twilio.js
@@ -0,0 +1,10 @@
+var twl = require('../logic/twilio/generateTwiml');
+var twilio = {
+	getTwiml: function (req, res) {
+		res.writeHead(200, {
+			'Content-Type': 'text/xml'
+		});
+		res.end(twl.getTwiml(req.params.userId, req.params.reminderId));
+	}
+}
+module.exports = twilio;
